01a56d39fbbbdb99068a3e3638f2adf049d8ae47
Takinsen <tandevill90@gmail.com>
2025-03-25 14:22:33 +0700

    setting vercel.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-slate-50" style={{paddingBottom:"30px"}}>
       <Banner/>
-      {/* Divider between Banner and RecommendList */}
       <hr className="my-6 border-t-2 border-gray-300" style={{ paddingBlock: "7px"}} />
       <RecommendList/>
       <FloatButton/>
